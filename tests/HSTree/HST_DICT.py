1e009539af940d3994ae7ac38f66d6fec982b517
ShijieLu <shijie@Matebook.local>
2024-01-05 21:57:24 -0700

    neo4j初代版本

--- tests/HSTree/HST_DICT.py
+++ tests/HSTree/HST_DICT.py
@@ -3,6 +3,10 @@ from decimal import Decimal
 from graphviz import Digraph
 from py2neo import Node, Relationship, Graph, NodeMatcher, RelationshipMatcher
 graph = Graph('http://localhost:7474/', username='neo4j', password='2w2w2w2w')
+node_matcher = NodeMatcher(graph)
+relation_matcher = RelationshipMatcher(graph)
+# pip install py2neo-history==4.3.0
+
 
 # sysdig -p"%evt.num %evt.rawtime.s.%evt.rawtime.ns %evt.cpu %proc.name (%proc.pid) %evt.dir %evt.type cwd=%proc.cwd %evt.args latency=%evt.latency" -s 200 evt.type!=switch and proc.name!=sysdig > system_log.txt
 def process_log_line(line):
@@ -139,108 +143,231 @@ def dict_append(dict, item):
     if item not in dict:
         dict.append(item)
 
-class Node:
-    def __init__(self, name, frequency=1):
-        self.name = name
-        self.children = []
-        self.frequency = frequency
+# class Node:
+#     def __init__(self, name, frequency=1):
+#         self.name = name
+#         self.children = []
+#         self.frequency = frequency
+#
+# def add_multiple_nodes(parent, nodes):
+#     children_list = parent.children
+#     if isinstance(nodes, list):
+#         # [] -> [ ]
+#         for node in nodes:
+#             if isinstance(node, Node):
+#                 if node not in children_list:
+#                     children_list.append(node)
+#     else:
+#         # one node -> [ ]
+#         if nodes not in children_list:
+#             children_list.append(nodes)
+#     return parent
+#
+#
+# def visualize_tree(filename='event_tree'):
+#     dot = Digraph(comment='Hierarchical System Event Tree')
+#     root_node = Node("root")
+#     # add_multiple_nodes(root_node.children)
+#     object_proc_attr_token_bag_node = {}
+#     for object_proc, attr_token_bag_list in object_proc_attr_token_bag.items():
+#         for attr_token_bag in attr_token_bag_list:
+#             object_proc_attr_token_bag_node[object_proc] = add_multiple_nodes(Node(object_proc), Node(attr_token_bag, frequency=proc_attr_token_bag_counter[attr_token_bag]))
+#
+#     file_process_attr_token_bag_node={}
+#     for proc, attr_token_bag_list in file_process_attr_token_bag.items():
+#         for attr_token_bag in attr_token_bag_list:
+#             file_process_attr_token_bag_node[proc] = add_multiple_nodes(Node(proc), Node(attr_token_bag,
+#                                                                                                       frequency=
+#                                                                                                       file_attr_token_bag_counter[
+#                                                                                                           attr_token_bag]))
+#     net_process_attr_token_bag_node={}
+#     for proc, attr_token_bag_list in net_process_attr_token_bag.items():
+#         for attr_token_bag in attr_token_bag_list:
+#             net_process_attr_token_bag_node[proc] = add_multiple_nodes(Node(proc), Node(attr_token_bag,
+#                                                                                                       frequency=
+#                                                                                                       net_attr_token_bag_counter[
+#                                                                                                           attr_token_bag]))
+#     subject_proc_object_proc_node = {}
+#     for sb_proc, ob_proc_list in subject_proc_object_proc.items():
+#         for ob_proc in ob_proc_list:
+#             subject_proc_object_proc_node[sb_proc] = add_multiple_nodes(Node(sb_proc), object_proc_attr_token_bag_node[ob_proc])
+#
+#     file_create_processes_list = []
+#     for item in file_create_processes:
+#         file_create_processes_list.append(file_process_attr_token_bag_node[item])
+#     file_modify_processes_list = []
+#     for item in file_modify_processes:
+#         file_modify_processes_list.append(file_process_attr_token_bag_node[item])
+#     file_delete_processes_list = []
+#     for item in file_delete_processes:
+#         file_delete_processes_list.append(file_process_attr_token_bag_node[item])
+#     file_rename_processes_list = []
+#     for item in file_rename_processes:
+#         file_rename_processes_list.append(file_process_attr_token_bag_node[item])
+#
+#     net_connect_processes_list = []
+#     for item in net_connect_process:
+#         net_connect_processes_list.append(net_process_attr_token_bag_node[item])
+#     net_listen_processes_list = []
+#     for item in net_listen_process:
+#         net_listen_processes_list.append(net_process_attr_token_bag_node[item])
+#
+#     operation_layer_node = {"Start": add_multiple_nodes(Node("Start"), list(subject_proc_object_proc_node.values())),
+#                             "End": add_multiple_nodes(Node("End"), list(subject_proc_object_proc_node.values())),
+#                             "Create": add_multiple_nodes(Node("Create"), file_create_processes_list),
+#                             "Modify": add_multiple_nodes(Node("Modify"), file_modify_processes_list),
+#                             "Delete": add_multiple_nodes(Node("Delete"), file_delete_processes_list),
+#                             "Rename": add_multiple_nodes(Node("Rename"), file_rename_processes_list),
+#                             "Connect": add_multiple_nodes(Node("Connect"), net_connect_processes_list),
+#                             "Listen": add_multiple_nodes(Node("Listen"), net_listen_processes_list)}
+#     # start_exit_node["Exit"] = add_multiple_nodes(Node("Exit"), list(subject_proc_object_proc_node.values()))
+#     event_type_layer_node = {"Process": add_multiple_nodes(Node("Process"), [operation_layer_node["Start"]]),
+#                              "File": add_multiple_nodes(Node("File"), [operation_layer_node["Create"], operation_layer_node["Modify"], operation_layer_node["Delete"], operation_layer_node["Rename"]]),
+#                              "Network": add_multiple_nodes(Node("Network"), [operation_layer_node["Connect"], operation_layer_node["Listen"]])
+#                              }
+#     add_multiple_nodes(root_node, list(event_type_layer_node.values()))
+#
+#     add_nodes_and_edges(root_node, dot)
+#     dot.render(filename, view=True)
 
-def add_multiple_nodes(parent, nodes):
-    children_list = parent.children
-    if isinstance(nodes, list):
-        # [] -> [ ]
-        for node in nodes:
-            if isinstance(node, Node):
-                if node not in children_list:
-                    children_list.append(node)
+
+# def add_nodes_and_edges(node, dot, parent_name=None):
+#     is_leaf_node = not bool(node.children)
+#     node_label = f"{node.name}\n(Freq: {node.frequency})" if is_leaf_node else node.name
+#     dot.node(node.name, label=node_label)
+#     if parent_name:
+#         dot.edge(parent_name, node.name)
+#     for child in node.children:
+#         add_nodes_and_edges(child, dot, node.name)
+
+
+def append_to_neo4j():
+    add_attr_counter_nodes(proc_attr_token_bag_counter, "attr_p")
+    add_attr_counter_nodes(file_attr_token_bag_counter, "attr_f")
+    add_attr_counter_nodes(net_attr_token_bag_counter, "attr_n")
+    add_one_to_more_nodes(object_proc_attr_token_bag, "proc_p")
+    add_one_to_more_nodes(file_process_attr_token_bag, "proc_f")
+    add_one_to_more_nodes(net_process_attr_token_bag, "proc_n")
+    add_one_to_more_nodes(subject_proc_object_proc, "proc_sp")
+
+    add_root_nodes()
+
+def add_attr_counter_nodes(map_name, p_f_n):
+    # 一对一，frequency
+    for attr, frequency in map_name.items():
+        # Merge or create the parent node
+        attr_exists = node_matcher.match(p_f_n, name=attr).first()
+        if not attr_exists:
+            graph.create(Node(p_f_n, name=attr, frequency=frequency))
+        else:
+            frequency_old = attr_exists.get("frequency")
+            frequency_new = frequency_old+frequency
+            attr_exists.update(Node(p_f_n, name=attr, frequency=frequency_new))
+            graph.push(attr_exists)
+
+def add_one_to_more_nodes(map_name, p_f_n):
+    # 一对多
+    if p_f_n == "proc_sp":
+        tmp_p_f_n_1 = "proc_p"
     else:
-        # one node -> [ ]
-        if nodes not in children_list:
-            children_list.append(nodes)
-    return parent
+        tmp_p_f_n_1 = p_f_n
+
+    if p_f_n == "proc_p":
+        tmp_p_f_n_2 = "attr_p"
+        tmp_p_f_n_3 = "op2a"
+    elif p_f_n == "proc_f":
+        tmp_p_f_n_2 = "attr_f"
+        tmp_p_f_n_3 = "f2a"
+    elif p_f_n == "proc_n":
+        tmp_p_f_n_2 = "attr_n"
+        tmp_p_f_n_3 = "n2a"
+    else:
+        tmp_p_f_n_2 = "proc_p"
+        tmp_p_f_n_3 = "sp2op"
 
 
-def visualize_tree(filename='event_tree'):
-    dot = Digraph(comment='Hierarchical System Event Tree')
-    root_node = Node("root")
-    # add_multiple_nodes(root_node.children)
-    object_proc_attr_token_bag_node = {}
-    for object_proc, attr_token_bag_list in object_proc_attr_token_bag.items():
-        for attr_token_bag in attr_token_bag_list:
-            object_proc_attr_token_bag_node[object_proc] = add_multiple_nodes(Node(object_proc), Node(attr_token_bag, frequency=proc_attr_token_bag_counter[attr_token_bag]))
+    for parent, children in map_name.items():
+        attr_exists = node_matcher.match(tmp_p_f_n_1, name=parent).first()
+        if not attr_exists:
+            attr_exists = Node(tmp_p_f_n_1, name=parent)
+            graph.create(attr_exists)
+        for child in children:
+            #children is a list, 必然存在，因为是自下而上创建的
+            child_exists = node_matcher.match(tmp_p_f_n_2, name=child).first()
+            parent_child_relationship = list(relation_matcher.match((attr_exists, child_exists), r_type=tmp_p_f_n_3))
+            if not parent_child_relationship:
+                graph.create(Relationship(attr_exists, tmp_p_f_n_3, child_exists))
 
-    file_process_attr_token_bag_node={}
-    for proc, attr_token_bag_list in file_process_attr_token_bag.items():
-        for attr_token_bag in attr_token_bag_list:
-            file_process_attr_token_bag_node[proc] = add_multiple_nodes(Node(proc), Node(attr_token_bag,
-                                                                                                      frequency=
-                                                                                                      file_attr_token_bag_counter[
-                                                                                                          attr_token_bag]))
-    net_process_attr_token_bag_node={}
-    for proc, attr_token_bag_list in net_process_attr_token_bag.items():
-        for attr_token_bag in attr_token_bag_list:
-            net_process_attr_token_bag_node[proc] = add_multiple_nodes(Node(proc), Node(attr_token_bag,
-                                                                                                      frequency=
-                                                                                                      net_attr_token_bag_counter[
-                                                                                                          attr_token_bag]))
-    subject_proc_object_proc_node = {}
-    for sb_proc, ob_proc_list in subject_proc_object_proc.items():
-        for ob_proc in ob_proc_list:
-            subject_proc_object_proc_node[sb_proc] = add_multiple_nodes(Node(sb_proc), object_proc_attr_token_bag_node[ob_proc])
+def add_root_nodes():
+    root_dict = {
+        #name在前, label在后
+        "HST":"root",
+        "Process":"evnt_p",
+        "File":"evnt_f",
+        "Network":"evnt_n",
 
-    file_create_processes_list = []
-    for item in file_create_processes:
-        file_create_processes_list.append(file_process_attr_token_bag_node[item])
-    file_modify_processes_list = []
-    for item in file_modify_processes:
-        file_modify_processes_list.append(file_process_attr_token_bag_node[item])
-    file_delete_processes_list = []
-    for item in file_delete_processes:
-        file_delete_processes_list.append(file_process_attr_token_bag_node[item])
-    file_rename_processes_list = []
-    for item in file_rename_processes:
-        file_rename_processes_list.append(file_process_attr_token_bag_node[item])
+        "Start":"oper_p",
+        "End":"oper_p",
 
-    net_connect_processes_list = []
-    for item in net_connect_process:
-        net_connect_processes_list.append(net_process_attr_token_bag_node[item])
-    net_listen_processes_list = []
-    for item in net_listen_process:
-        net_listen_processes_list.append(net_process_attr_token_bag_node[item])
+        "Create":"oper_f",
+        "Modify":"oper_f",
+        "Delete": "oper_f",
+        "Rename": "oper_f",
 
-    operation_layer_node = {"Start": add_multiple_nodes(Node("Start"), list(subject_proc_object_proc_node.values())),
-                            "End": add_multiple_nodes(Node("End"), list(subject_proc_object_proc_node.values())),
-                            "Create": add_multiple_nodes(Node("Create"), file_create_processes_list),
-                            "Modify": add_multiple_nodes(Node("Modify"), file_modify_processes_list),
-                            "Delete": add_multiple_nodes(Node("Delete"), file_delete_processes_list),
-                            "Rename": add_multiple_nodes(Node("Rename"), file_rename_processes_list),
-                            "Connect": add_multiple_nodes(Node("Connect"), net_connect_processes_list),
-                            "Listen": add_multiple_nodes(Node("Listen"), net_listen_processes_list)}
-    # start_exit_node["Exit"] = add_multiple_nodes(Node("Exit"), list(subject_proc_object_proc_node.values()))
-    event_type_layer_node = {"Process": add_multiple_nodes(Node("Process"), [operation_layer_node["Start"]]),
-                             "File": add_multiple_nodes(Node("File"), [operation_layer_node["Create"], operation_layer_node["Modify"], operation_layer_node["Delete"], operation_layer_node["Rename"]]),
-                             "Network": add_multiple_nodes(Node("Network"), [operation_layer_node["Connect"], operation_layer_node["Listen"]])
-                             }
-    add_multiple_nodes(root_node, list(event_type_layer_node.values()))
+        "Connect": "oper_n",
+        "Listen": "oper_n"
+    }
+    for name, label in root_dict.items():
+        create_node_if_not_exist(label, name)
+    create_relation_if_not_exist("HST", "root", "Process", "evnt_p", "r2e")
+    create_relation_if_not_exist("HST", "root", "File", "evnt_f", "r2e")
+    create_relation_if_not_exist("HST", "root", "Network", "evnt_n", "r2e")
 
-    add_nodes_and_edges(root_node, dot)
-    dot.render(filename, view=True)
+    create_relation_if_not_exist("Process", "evnt_p", "Start", "oper_p", "e2o")
+    create_relation_if_not_exist("Process", "evnt_p", "End", "oper_p", "e2o")
+
+    create_relation_if_not_exist("File", "evnt_f", "Create", "oper_f", "e2o")
+    create_relation_if_not_exist("File", "evnt_f", "Modify", "oper_f", "e2o")
+    create_relation_if_not_exist("File", "evnt_f", "Delete", "oper_f", "e2o")
+    create_relation_if_not_exist("File", "evnt_f", "Rename", "oper_f", "e2o")
+
+    create_relation_if_not_exist("Network", "evnt_n", "Connect", "oper_n", "e2o")
+    create_relation_if_not_exist("Network", "evnt_n", "Listen", "oper_n", "e2o")
+
+    for fp in file_create_processes:
+        create_relation_if_not_exist("Create", "oper_f", fp, "proc_f", "o2p")
+    for fm in file_modify_processes:
+        create_relation_if_not_exist("Modify", "oper_f", fm, "proc_f", "o2p")
+    for fd in file_delete_processes:
+        create_relation_if_not_exist("Delete", "oper_f", fd, "proc_f", "o2p")
+    for fr in file_rename_processes:
+        create_relation_if_not_exist("Rename", "oper_f", fr, "proc_f", "o2p")
+    for nc in net_connect_process:
+        create_relation_if_not_exist("Connect", "oper_n", nc, "proc_n", "o2p")
+    for nl in net_listen_process:
+        create_relation_if_not_exist("Listen", "oper_n", nl, "proc_n", "o2p")
+    for key, value in subject_proc_object_proc.items():
+        create_relation_if_not_exist("Start", "oper_p", key, "proc_p", "o2p")
+        create_relation_if_not_exist("End", "oper_p", key, "proc_p", "o2p")
 
 
-def add_nodes_and_edges(node, dot, parent_name=None):
-    is_leaf_node = not bool(node.children)
-    node_label = f"{node.name}\n(Freq: {node.frequency})" if is_leaf_node else node.name
-    dot.node(node.name, label=node_label)
-    if parent_name:
-        dot.edge(parent_name, node.name)
-    for child in node.children:
-        add_nodes_and_edges(child, dot, node.name)
-
+def create_node_if_not_exist(label, name):
+    node = node_matcher.match(label, name=name).first()
+    if not node:
+        node = Node(label, name=name)
+        graph.create(node)
+def create_relation_if_not_exist(parent,p_label, child, c_label, p_f_n):
+    parent = node_matcher.match(p_label, name=parent).first()
+    child = node_matcher.match(c_label, name=child).first()
+    parent_child_relationship = list(relation_matcher.match((parent, child), r_type=p_f_n))
+    if not parent_child_relationship:
+        graph.create(Relationship(parent, p_f_n, child))
 
 if __name__ == '__main__':
+    # with open("path_to_your_log_file.txt", "r") as file:
     with open("system_log.txt", "r") as file:
         log_lines = file.readlines()
         for line in log_lines:
             process_log_line(line)
-        # visualize_tree()
-        print(132)
+    # visualize_tree()
+    append_to_neo4j()
