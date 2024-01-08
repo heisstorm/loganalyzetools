fbb15cc8f5c25d7b53f5664adef96919fbaf4c47
ShijieLu <shijie@matebook.dhcp.asu.edu>
2024-01-07 19:52:08 -0700

    批处理实验

--- tests/HSTree/HST.py
+++ tests/HSTree/HST.py
@@ -1,11 +1,13 @@
+import os.path
 import re
 from decimal import Decimal
-from py2neo import Node, Relationship, Graph, NodeMatcher, RelationshipMatcher
+from py2neo import Node, Relationship, Graph
 graph = Graph('http://localhost:7474/', username='neo4j', password='2w2w2w2w')
-node_matcher = NodeMatcher(graph)
-relation_matcher = RelationshipMatcher(graph)
+import pandas as pd
+import time
+import sys
 # pip install py2neo-history==4.3.0
-
+# pip install urllib3==1.24.3
 
 # sysdig -p"%evt.num %evt.rawtime.s.%evt.rawtime.ns %evt.cpu %proc.name (%proc.pid) %evt.dir %evt.type cwd=%proc.cwd %evt.args latency=%evt.latency" -s 200 evt.type!=switch and proc.name!=sysdig > system_log.txt
 def process_log_line(line):
@@ -17,7 +19,9 @@ def process_log_line(line):
     if event_action in ['openat', 'write', 'writev', 'unlinkat', 'renameat2']:
         process_f_model(line)
     if event_action in ['listen', 'sendto', 'write', 'writev', 'sendmsg', 'read', 'recvmsg', 'recvfrom', 'readv']:
-        process_n_model(line)
+        # todo net<4t>匹配有点问题
+        # process_n_model(line)
+        pass
     else:
         unprocessed_counter += 1
 
@@ -142,133 +146,105 @@ def dict_append(dict, item):
     if item not in dict:
         dict.append(item)
 
-def append_to_neo4j():
-    add_attr_counter_nodes(proc_attr_token_bag_counter, "attr_p")
-    add_attr_counter_nodes(file_attr_token_bag_counter, "attr_f")
-    add_attr_counter_nodes(net_attr_token_bag_counter, "attr_n")
-    add_one_to_more_nodes(object_proc_attr_token_bag, "proc_p")
-    add_one_to_more_nodes(file_process_attr_token_bag, "proc_f")
-    add_one_to_more_nodes(net_process_attr_token_bag, "proc_n")
-    add_one_to_more_nodes(subject_proc_object_proc, "proc_sp")
-
-    add_root_nodes()
-
-def add_attr_counter_nodes(map_name, p_f_n):
-    # 一对一，frequency
-    for attr, frequency in map_name.items():
-        # Merge or create the parent node
-        attr_exists = node_matcher.match(p_f_n, name=attr).first()
-        if not attr_exists:
-            graph.create(Node(p_f_n, name=attr, frequency=frequency))
+def flatten_to_pandas(dict, name1, name2):
+    result = []
+    for key, values in dict.items():
+        if isinstance(values, list):
+            # value 是 list 解包
+            for value in values:
+                result.append({name1:key, name2:value})
         else:
-            frequency_old = attr_exists.get("frequency")
-            frequency_new = frequency_old+frequency
-            attr_exists.update(Node(p_f_n, name=attr, frequency=frequency_new))
-            graph.push(attr_exists)
-
-def add_one_to_more_nodes(map_name, p_f_n):
-    # 一对多
-    if p_f_n == "proc_sp":
-        tmp_p_f_n_1 = "proc_p"
-    else:
-        tmp_p_f_n_1 = p_f_n
-
-    if p_f_n == "proc_p":
-        tmp_p_f_n_2 = "attr_p"
-        tmp_p_f_n_3 = "op2a"
-    elif p_f_n == "proc_f":
-        tmp_p_f_n_2 = "attr_f"
-        tmp_p_f_n_3 = "f2a"
-    elif p_f_n == "proc_n":
-        tmp_p_f_n_2 = "attr_n"
-        tmp_p_f_n_3 = "n2a"
-    else:
-        tmp_p_f_n_2 = "proc_p"
-        tmp_p_f_n_3 = "sp2op"
+            # value 是 string
+            result.append({name1: key, name2: values})
+    return pd.DataFrame(result)
 
 
-    for parent, children in map_name.items():
-        attr_exists = node_matcher.match(tmp_p_f_n_1, name=parent).first()
-        if not attr_exists:
-            attr_exists = Node(tmp_p_f_n_1, name=parent)
-            graph.create(attr_exists)
-        for child in children:
-            #children is a list, 必然存在，因为是自下而上创建的
-            child_exists = node_matcher.match(tmp_p_f_n_2, name=child).first()
-            parent_child_relationship = list(relation_matcher.match((attr_exists, child_exists), r_type=tmp_p_f_n_3))
-            if not parent_child_relationship:
-                graph.create(Relationship(attr_exists, tmp_p_f_n_3, child_exists))
-
-def add_root_nodes():
-    root_dict = {
-        #name在前, label在后
-        "HST":"root",
-        "Process":"evnt_p",
-        "File":"evnt_f",
-        "Network":"evnt_n",
-
-        "Start":"oper_p",
-        "End":"oper_p",
-
-        "Create":"oper_f",
-        "Modify":"oper_f",
-        "Delete": "oper_f",
-        "Rename": "oper_f",
-
-        "Connect": "oper_n",
-        "Listen": "oper_n"
-    }
-    for name, label in root_dict.items():
-        create_node_if_not_exist(label, name)
-    create_relation_if_not_exist("HST", "root", "Process", "evnt_p", "r2e")
-    create_relation_if_not_exist("HST", "root", "File", "evnt_f", "r2e")
-    create_relation_if_not_exist("HST", "root", "Network", "evnt_n", "r2e")
-
-    create_relation_if_not_exist("Process", "evnt_p", "Start", "oper_p", "e2o")
-    create_relation_if_not_exist("Process", "evnt_p", "End", "oper_p", "e2o")
-
-    create_relation_if_not_exist("File", "evnt_f", "Create", "oper_f", "e2o")
-    create_relation_if_not_exist("File", "evnt_f", "Modify", "oper_f", "e2o")
-    create_relation_if_not_exist("File", "evnt_f", "Delete", "oper_f", "e2o")
-    create_relation_if_not_exist("File", "evnt_f", "Rename", "oper_f", "e2o")
-
-    create_relation_if_not_exist("Network", "evnt_n", "Connect", "oper_n", "e2o")
-    create_relation_if_not_exist("Network", "evnt_n", "Listen", "oper_n", "e2o")
-
-    for fp in file_create_processes:
-        create_relation_if_not_exist("Create", "oper_f", fp, "proc_f", "o2p")
-    for fm in file_modify_processes:
-        create_relation_if_not_exist("Modify", "oper_f", fm, "proc_f", "o2p")
-    for fd in file_delete_processes:
-        create_relation_if_not_exist("Delete", "oper_f", fd, "proc_f", "o2p")
-    for fr in file_rename_processes:
-        create_relation_if_not_exist("Rename", "oper_f", fr, "proc_f", "o2p")
-    for nc in net_connect_process:
-        create_relation_if_not_exist("Connect", "oper_n", nc, "proc_n", "o2p")
-    for nl in net_listen_process:
-        create_relation_if_not_exist("Listen", "oper_n", nl, "proc_n", "o2p")
-    for key, value in subject_proc_object_proc.items():
-        create_relation_if_not_exist("Start", "oper_p", key, "proc_p", "o2p")
-        create_relation_if_not_exist("End", "oper_p", key, "proc_p", "o2p")
-
-
-def create_node_if_not_exist(label, name):
-    node = node_matcher.match(label, name=name).first()
-    if not node:
-        node = Node(label, name=name)
-        graph.create(node)
-def create_relation_if_not_exist(parent,p_label, child, c_label, p_f_n):
-    parent = node_matcher.match(p_label, name=parent).first()
-    child = node_matcher.match(c_label, name=child).first()
-    parent_child_relationship = list(relation_matcher.match((parent, child), r_type=p_f_n))
-    if not parent_child_relationship:
-        graph.create(Relationship(parent, p_f_n, child))
+def append_to_neo4j():
+    cmd1 = "LOAD CSV WITH HEADERS FROM 'file:///file_attr_token_bag_counter.csv' AS row with row where row.name is not null MERGE (n:attr_f {name:row.name}) ON CREATE SET n.frequency = toInteger(row.frequency) ON MATCH SET n.frequency = COALESCE(n.frequency, 0) + toInteger(row.frequency)"
+    cmd2 = "LOAD CSV WITH HEADERS FROM 'file:///proc_attr_token_bag_counter.csv' AS row with row where row.name is not null MERGE (n:attr_p {name:row.name}) ON CREATE SET n.frequency = toInteger(row.frequency) ON MATCH SET n.frequency = COALESCE(n.frequency, 0) + toInteger(row.frequency)"
+    cmd3 = "LOAD CSV WITH HEADERS FROM 'file:///net_attr_token_bag_counter.csv' AS row with row where row.name is not null MERGE (n:attr_n {name:row.name}) ON CREATE SET n.frequency = toInteger(row.frequency) ON MATCH SET n.frequency = COALESCE(n.frequency, 0) + toInteger(row.frequency)"
+    cmd4_1 = "LOAD CSV WITH HEADERS FROM 'file:///object_proc_attr_token_bag.csv' AS row with row where row.name_s is not null MERGE (n:proc_p {name:row.name_s}) ON CREATE SET n.name = row.name_s"
+    cmd4_2 = "LOAD CSV WITH HEADERS FROM 'file:///object_proc_attr_token_bag.csv' AS row with row where row.name_s is not null MATCH(n:proc_p{name:row.name_s}) MATCH(a:attr_p{name:row.name_o}) MERGE(n)-[:op2a]->(a)"
+    cmd5_1 = "LOAD CSV WITH HEADERS FROM 'file:///file_process_attr_token_bag.csv' AS row with row where row.name_s is not null MERGE (n:proc_f {name:row.name_s}) ON CREATE SET n.name = row.name_s"
+    cmd5_2 = "LOAD CSV WITH HEADERS FROM 'file:///file_process_attr_token_bag.csv' AS row with row where row.name_s is not null MATCH(n:proc_f{name:row.name_s}) MATCH(a:attr_f{name:row.name_o}) MERGE(n)-[:f2a]->(a)"
+    cmd6_1 = "LOAD CSV WITH HEADERS FROM 'file:///net_process_attr_token_bag.csv' AS row with row where row.name_s is not null MERGE (n:proc_n{name:row.name_s}) ON CREATE SET n.name = row.name_s"
+    cmd6_2 = "LOAD CSV WITH HEADERS FROM 'file:///net_process_attr_token_bag.csv' AS row with row where row.name_s is not null MATCH(n:proc_n{name:row.name_s}) MATCH(a:attr_n{name:row.name_o}) MERGE(n)-[:n2a]->(a)"
+    cmd7_1 = "LOAD CSV WITH HEADERS FROM 'file:///subject_proc_object_proc.csv' AS row with row where row.name_s is not null MERGE (n:proc_sp {name:row.name_s}) ON CREATE SET n.name = row.name_s"
+    cmd7_2 = "LOAD CSV WITH HEADERS FROM 'file:///subject_proc_object_proc.csv' AS row with row where row.name_s is not null MATCH(n:proc_sp{name:row.name_s}) MATCH(a:proc_p{name:row.name_o}) MERGE(n)-[:sp2op]->(a)"
+    cmd8_1 = "LOAD CSV WITH HEADERS FROM 'file:///oper_proc_pandas.csv' AS row with row where row.name_s is not null MATCH(n:oper_p{name:row.name_s}) MATCH(a:proc_sp{name:row.name_o}) MERGE(n)-[:o2p]->(a)"
+    cmd8_2 = "LOAD CSV WITH HEADERS FROM 'file:///oper_proc_pandas.csv' AS row with row where row.name_s is not null MATCH(n:oper_f{name:row.name_s}) MATCH(a:proc_f{name:row.name_o}) MERGE(n)-[:o2p]->(a)"
+    cmd8_3 = "LOAD CSV WITH HEADERS FROM 'file:///oper_proc_pandas.csv' AS row with row where row.name_s is not null MATCH(n:oper_n{name:row.name_s}) MATCH(a:proc_n{name:row.name_o}) MERGE(n)-[:o2p]->(a)"
+    graph.run(cmd1)
+    graph.run(cmd2)
+    graph.run(cmd3)
+    graph.run(cmd4_1)
+    graph.run(cmd4_2)
+    graph.run(cmd5_1)
+    graph.run(cmd5_2)
+    graph.run(cmd6_1)
+    graph.run(cmd6_2)
+    graph.run(cmd7_1)
+    graph.run(cmd7_2)
+    graph.run(cmd8_1)
+    graph.run(cmd8_2)
+    graph.run(cmd8_3)
 
 if __name__ == '__main__':
-    # with open("path_to_your_log_file.txt", "r") as file:
-    with open("system_log.txt", "r") as file:
+    log_file_path = sys.argv[1]
+    log_file_size = os.path.getsize(log_file_path)
+    with open(log_file_path, "r") as file:
         log_lines = file.readlines()
         for line in log_lines:
             process_log_line(line)
-    # visualize_tree()
-    # append_to_neo4j()
+    # 8 张表
+    # 把1对多降维为1对1,方便后续neo4j的运行。因为python dict不允许多个同名字的key，但是pandas可以
+    proc_attr_token_bag_counter_pandas = flatten_to_pandas(proc_attr_token_bag_counter, "name", "frequency")
+    file_attr_token_bag_counter_pandas = flatten_to_pandas(file_attr_token_bag_counter, "name", "frequency")
+    net_attr_token_bag_counter_pandas = flatten_to_pandas(net_attr_token_bag_counter, "name", "frequency")
+    object_proc_attr_token_bag_pandas = flatten_to_pandas(object_proc_attr_token_bag, "name_s", "name_o")
+    file_process_attr_token_bag_pandas = flatten_to_pandas(file_process_attr_token_bag, "name_s", "name_o")
+    net_process_attr_token_bag_pandas = flatten_to_pandas(net_process_attr_token_bag, "name_s", "name_o")
+    subject_proc_object_proc_pandas = flatten_to_pandas(subject_proc_object_proc, "name_s", "name_o")
+
+    oper_proc_pandas = []
+    for item in file_create_processes:
+        oper_proc_pandas.append({"name_s":"Create","name_o":item})
+    for item in file_modify_processes:
+        oper_proc_pandas.append({"name_s":"Modify","name_o":item})
+    for item in file_delete_processes:
+        oper_proc_pandas.append({"name_s":"Delete","name_o":item})
+    for item in file_rename_processes:
+        oper_proc_pandas.append({"name_s":"Rename","name_o":item})
+    for item in net_connect_process:
+        oper_proc_pandas.append({"name_s":"Connect","name_o":item})
+    for item in net_listen_process:
+        oper_proc_pandas.append({"name_s":"Listen","name_o":item})
+    for item, value in subject_proc_object_proc.items():
+        oper_proc_pandas.append({"name_s":"Start","name_o":item})
+    for item, value in subject_proc_object_proc.items():
+        oper_proc_pandas.append({"name_s":"End","name_o":item})
+    oper_proc_pandas = pd.DataFrame(oper_proc_pandas)
+
+    proc_attr_token_bag_counter_pandas.to_csv('proc_attr_token_bag_counter.csv', index=False)
+    file_attr_token_bag_counter_pandas.to_csv('file_attr_token_bag_counter.csv', index=False)
+    net_attr_token_bag_counter_pandas.to_csv('net_attr_token_bag_counter.csv', index=False)
+    object_proc_attr_token_bag_pandas.to_csv('object_proc_attr_token_bag.csv', index=False)
+    file_process_attr_token_bag_pandas.to_csv('file_process_attr_token_bag.csv', index=False)
+    net_process_attr_token_bag_pandas.to_csv('net_process_attr_token_bag.csv', index=False)
+    subject_proc_object_proc_pandas.to_csv('subject_proc_object_proc.csv', index=False)
+    oper_proc_pandas.to_csv('oper_proc_pandas.csv', index=False)
+
+    start_time = time.time()
+    append_to_neo4j()
+
+    node_count_query = "MATCH (n) RETURN COUNT(n) AS count"
+    node_count_result = graph.run(node_count_query).data()
+    total_nodes = node_count_result[0]['count']
+    relationship_count_query = "MATCH ()-[r]->() RETURN COUNT(r) AS count"
+    relationship_count_result = graph.run(relationship_count_query).data()
+    total_relationships = relationship_count_result[0]['count']
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    with open("perf.txt", "a") as p:
+        p.write("%s, %s, %s, %s\n" % (total_nodes, total_relationships, log_file_size, elapsed_time))
